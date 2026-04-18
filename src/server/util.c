a426b9e99a04e7666bce2ed86a44cc6e331835cf
RomanDonw <romandonw628@gmail.com>
2026-04-18 19:58:08 +1000

    added safe variants for malloc/realloc functions

--- src/server/util.c
+++ src/server/util.c
@@ -8,3 +8,17 @@ void handlesockerr(const char *funcname)
     printf("%s error: %s. Application aborted.\n", funcname, socket_strerror(socket_getlasterror()));
     abort();
 }
+
+void *malloc_s(size_t size)
+{
+    void *ret = malloc(size);
+    if (!ret) { puts("Memory allocation failed. Application terminated."); abort(); }
+    return ret;
+}
+
+void *realloc_s(void *ptr, size_t size)
+{
+    void *ret = realloc(ptr, size);
+    if (!ret) { puts("Memory allocation failed. Application terminated."); abort(); }
+    return ret;
+}
--- src/server/util.h
+++ src/server/util.h
@@ -3,4 +3,7 @@
 
 void handlesockerr(const char *funcname);
 
+void *malloc_s(size_t size);
+void *realloc_s(void *ptr, size_t size);
+
 #endif
